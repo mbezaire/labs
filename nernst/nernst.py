e966e9324e12e24af43dfb415489127220ace0a9
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-07-13 11:19:47 -0400

    Update nernst.py

--- nernst/nernst.py
+++ nernst/nernst.py
@@ -1,9 +1,7 @@
 """
-Computational Lab 2: Sea slug skeleton
+Computational Lab 1: Nernst Equation
 
-Starter File by mbezaire@bu.edu, based on Tom Anastasio's
-habituation model in MATLAB, which sets up a simple simulation
-of the Aplysia gill withdrawal reflex with desensitization
+Starter File by mbezaire@bu.edu
 
 Program completed by:
 Date: 
