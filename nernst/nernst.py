8c604f2300f9464716267488e27ff69086dbfde3
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-07-13 06:54:17 -0400

    placeholder for nernst lab

--- nernst/nernst.py
+++ nernst/nernst.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 Computational Lab 2: Sea slug skeleton
 
@@ -8,3 +9,15 @@ of the Aplysia gill withdrawal reflex with desensitization
 Program completed by:
 Date: 
 """
+=======
+"""
+Computational Lab 2: Sea slug skeleton
+
+Starter File by mbezaire@bu.edu, based on Tom Anastasio's
+habituation model in MATLAB, which sets up a simple simulation
+of the Aplysia gill withdrawal reflex with desensitization
+
+Program completed by:
+Date: 
+"""
+>>>>>>> placeholder for nernst lab
